6e414ca177c3f4a7e7847f43a6aac3a20aa6e724
mzsn <mzsn@users.noreply.github.com>
2026-04-26 07:48:19 +0000

    Revert "feat: add yotsuba and sakura pm2 configs to ecosystem.config.cjs"
    This reverts commit 70293ba585e1387f7d5bbe4bd65858d43315e867.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,25 +9,5 @@ module.exports = {
     env: {
       NODE_ENV: "development",
     }
-  },{
-    name: "yotsuba",
-    script: "npx",
-    args: "tsx --env-file=.env.yotsuba src/index.ts",
-    interpreter: "none",
-    watch: false,
-    kill_timeout: 10000,
-    env: {
-      NODE_ENV: "development",
-    }
-  },{
-    name: "sakura",
-    script: "npx",
-    args: "tsx --env-file=.env.sakura src/index.ts",
-    interpreter: "none",
-    watch: false,
-    kill_timeout: 10000,
-    env: {
-      NODE_ENV: "development",
-    }
   }]
 };
